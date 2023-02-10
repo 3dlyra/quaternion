8c225b75005f1725edea8b4840e3d138c119aab1
3Dlyra <3dlyra.com@gmail.com>
2023-02-10 22:54:51 +0500

    Implement `rotateX` method

--- quaternion.d.ts
+++ quaternion.d.ts
@@ -1,1 +1,2 @@
 export function create( x?: number, y?: number, z?: number, w?: number ): Float32Array;
+export function rotateX( q: Float32Array, radian: number ): Float32Array;
--- quaternion.js
+++ quaternion.js
@@ -2,3 +2,13 @@ export function create( x, y, z, w ) {
 
 	return new Float32Array( [ x || 0, y || 0, z || 0, w || 1 ] )
 }
+
+export function rotateX( q, radian ) {
+
+	return new Float32Array( [
+		q[ 0 ] * Math.cos( radian * 0.5 ) + q[ 3 ] * Math.sin( radian * 0.5 ),
+		q[ 1 ] * Math.cos( radian * 0.5 ) + q[ 2 ] * Math.sin( radian * 0.5 ),
+		q[ 2 ] * Math.cos( radian * 0.5 ) - q[ 1 ] * Math.sin( radian * 0.5 ),
+		q[ 3 ] * Math.cos( radian * 0.5 ) - q[ 0 ] * Math.sin( radian * 0.5 )
+	] )
+}
